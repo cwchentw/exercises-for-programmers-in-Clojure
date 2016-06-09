3aecb542bb8e136f20bada3393e602f69e47f01a
Michael Chen <cwchen123@gmail.com>
2016-06-09 08:33:49 +0800

    Add exercise 3

---
+++ ex03-print-quotes/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ex03-print-quotes
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ ex03-print-quotes/src/ex03_print_quotes/core.clj
@@ -0,0 +1,10 @@
+(ns ex03-print-quotes.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "What is the quote? These aren't the droids you're looking for.
+Who said it? Obi-Wan Kenobi
+Obi-Wan Kenobi says, \"These aren't the droids
+you're looking for.\""))
